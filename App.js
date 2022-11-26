d8a021de2461b359b6db59fa0000667ff7e314f3
Aleksi Nokela <n0noal00@students.oamk.fi>
2022-11-26 19:20:25 +0200

    push to repository

--- App.js
+++ App.js
@@ -1,11 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [screenOrientation, setScreenOrientation] = useState('portrait');
+  const [isPortrait, setIsPortrait] = useState(true);
+
+  useEffect(() => {
+    const subscription = ScreenOrientation.addOrientationChangeListener((value) => {
+      if (value.orientationInfo.orientation === ScreenOrientation.Orientation.PORTRAIT_UP) {
+        setScreenOrientation('portrait');
+        setIsPortrait(true);
+      } else if ( value.orientationInfo.orientation === ScreenOrientation.Orientation.LANDSCAPE_LEFT) {
+        setScreenOrientation('landscape');
+        setIsPortrait(false);
+      }
+    });
+    return () => {
+      ScreenOrientation.removeOrientationChangeListener(subscription);
+    };
+
+  }, []);
+  
+
+  const lockToPortrait = async () => {
+    await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, isPortrait ? styles.portrait : styles.landscape]}>
+      <Text>{screenOrientation}</Text>
+
+    {(() => {
+      if (isPortrait) {
+        return <Text>This is for the portrait.</Text>
+      } else {
+        return <Text>This is for the Landscape</Text>
+      }
+    })()}
+
+      <Button onPress={lockToPortrait} title='Lock to portrait'/>
+      
     </View>
   );
 }
@@ -17,4 +52,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  portrait: {
+    backgroundColor: '#ccc',
+  },
+  landscape: {
+    backgroundColor: '#999',
+  }
 });
